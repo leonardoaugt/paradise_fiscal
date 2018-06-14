65ce797e569f772c374f3c1f85ebe39c55883d18
leonardoaugt <leonardoaugt@hotmail.com>
2018-06-14 12:18:52 -0300

    Base files

---
+++ src/paradise_fiscal/paradise_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ParadiseApiConfig(AppConfig):
+    name = 'paradise_api'
---
+++ src/paradise_fiscal/paradise_api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+class Nfes(APIView):
+    def get(self, request):
+
+        return
+
