3ca9d93b9a2abeba9a56d9fff4185168e0016206
leonardoaugt <leonardoaugt@hotmail.com>
2018-06-14 13:50:15 -0300

    Watching nfes_all method return

--- src/paradise_fiscal/paradise_api/service/nfe/nfe_parser_service.py
+++ src/paradise_fiscal/paradise_api/service/nfe/nfe_parser_service.py
@@ -1,14 +1,45 @@
-
 from .nfe_reader_service import read_file
 
 
 def parse_file():
 
+    root = {}
     data = read_file('NFe.txt')
-    data = nfes_all(data)
+    root = nfes_all(root, data)
+    return root
 
-def nfes_all(data):
 
+def nfes_all(root, data):
+
+    # Get all NFes
+    i = 0
     for row in data:
         row_splitted = row.strip().split(';')
-        return data
+        root = get_nfe(root, row_splitted, i)
+        i += 1
+    f = open('nfes_all.txt', 'w')
+    f.write(str(root))
+    f.close()
+
+
+def get_nfe(root, row_splitted, i):
+
+    # Make NFe object
+    root['NFe_{}'.format(i)] = {
+        'Data': row_splitted[0],
+        'Tipo': row_splitted[1],
+        'CnpjCpf': row_splitted[2],
+        'Numero': row_splitted[3],
+        'Serie': row_splitted[4],
+        'Modelo': row_splitted[5],
+        'Chave': row_splitted[6],
+        'ValorTotal': row_splitted[7],
+        'ValorProd': row_splitted[8],
+        'ValorICMS': row_splitted[9],
+        'ValorIPI': row_splitted[10],
+        'Status': row_splitted[11],
+        }
+    return root
+
+
+parse_file()
