82fcf9184b9c8d31f50ff423228e3baf2de1ec9a
leonardoaugt <leonardoaugt@hotmail.com>
2018-06-15 21:40:41 -0300

    NFeTran module added

---
+++ src/paradise_fiscal/paradise_api/service/nfetran/nfetran_service.py
@@ -0,0 +1,27 @@
+
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
+from rest_framework.authentication import get_authorization_header
+
+from .nfetran_parser_service import parse_file, get_by_key
+from ..nfe.nfe_parser_service import parse_file
+
+
+def get_nfetran():
+
+    data = parse_file()
+    if data:
+        return Response({'Dados': data}, HTTP_200_OK)
+    return Response({'Erro': 'Nenhum documento encontrado'}, HTTP_404_NOT_FOUND)
+
+
+def get_nfetran_key(key):
+
+    key = str(key)
+    nfetran = get_by_key(key)
+    if nfetran:
+        nfe_key = parse_file(key, 6)
+        if nfe_key:
+            merged = {**nfe_key, **nfetran}
+            return Response({'Dados': merged}, HTTP_200_OK)
+    return Response({'Erro': 'Documento não encontrado'}, HTTP_404_NOT_FOUND)
