fa0c120199111d567e83d3da1de714d45426479e
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-02-26 00:30:04 -0300

    Starting refactor

--- src/paradise_fiscal/paradise_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParadiseApiConfig(AppConfig):
-    name = 'paradise_api'
--- src/paradise_fiscal/paradise_api/service/utils/utils.py
+++
@@ -1,8 +0,0 @@
-
-def read_file(filename):
-
-    # Return an array of text
-    f = open(filename, 'r')
-    data = f.readlines()
-    f.close()
-    return data
