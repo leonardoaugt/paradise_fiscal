c753ec3fc9de351bac656064a618c379a4cd98ae
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-03-04 17:54:11 -0300

    Refactor

--- core/filter.py
+++ core/filter.py
@@ -3,18 +3,30 @@ from datetime import datetime
 
 class Filter:
     def __call__(self, doc, filters):
-        if doc and filters['Chave']:
-            if doc['Chave'] == filters['Chave']:
-                return doc
+        self.doc = doc
+        self.filters = filters
+        return self.doc and self.filter_by_key() and self.filter_by_date()
 
-        if doc and filters['DataInicio'] and filters['DataFim']:
+    def filter_by_date(self):
+        if not self.filters['DataInicio'] and not self.filters['DataFim']:
+            return self.doc
+
+        if self.filters['DataInicio'] and self.filters['DataFim']:
 
             def to_python(date):
                 return datetime.date(datetime.strptime(date, '%d/%m/%Y'))
 
-            start = to_python(filters['DataInicio'])
-            end = to_python(filters['DataFim'])
-            ref = to_python(doc['Data'])
+            start = to_python(self.filters['DataInicio'])
+            end = to_python(self.filters['DataFim'])
+            ref = to_python(self.doc['Data'])
 
             if start <= ref <= end:
-                return doc
+                return self.doc
+
+    def filter_by_key(self):
+        if not self.filters['Chave']:
+            return self.doc
+
+        if self.filters['Chave']:
+            if self.doc['Chave'] == self.filters['Chave']:
+                return self.doc
